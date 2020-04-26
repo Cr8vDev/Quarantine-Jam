31abf30fdea128d7053df7b27980f6f41d5cb0e0
Og Rafael <ografael86@gmail.com>
2020-04-25 21:04:48 -0300

    2D movement

---
+++ Assets/Scripts/Objects/Character.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Objects
+{
+  public class Character : MonoBehaviour
+  {
+    public float health = 100f;
+
+    private void Update()
+    {
+      if (IsDead())
+      {
+        Destroy(gameObject);
+      }
+    }
+
+    private bool IsDead()
+    {
+      return health <= 0;
+    }
+
+    // TODO make player take damage
+    // public void TakeDamage(float amount)
+    // {
+    //   health -= amount;
+    // }
+  }
+}
