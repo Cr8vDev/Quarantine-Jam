dd41b97ac5d2e93ac6a9a610b664ee47d807e680
Cr8v Dev <cr8vdev@gmail.com>
2020-04-26 23:05:28 +0200

    Improved Movement System

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ReferenceDirection
+{
+    Left,
+    Right
+}
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField]
+    private ReferenceDirection referenceDirection;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Animator animator;
+
+
+    
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        animator = GetComponent<Animator>();
+
+        animator.speed = 0f;
+    }
+
+
+
+    public void HorizontalAnimation(float dirX)
+    {
+        if(dirX > 0f)
+        {
+            animator.speed = 1f;
+
+            if (referenceDirection == ReferenceDirection.Left)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+
+            animator.SetBool("Horizontal", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+        }
+        else if(dirX < 0f)
+        {
+            animator.speed = 1f;
+
+            if (referenceDirection == ReferenceDirection.Left)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+
+            animator.SetBool("Horizontal", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+        }
+    }
+
+
+
+    public void VerticalAnimation(float dirY)
+    {
+        animator.speed = 1f;
+
+        if (dirY > 0.05f)
+        {
+            animator.SetBool("Up", true);
+            animator.SetBool("Horizontal", false);
+            animator.SetBool("Down", false);
+        }
+        else if (dirY < -0.05f)
+        {
+            animator.SetBool("Down", true);
+            animator.SetBool("Horizontal", false);
+            animator.SetBool("Up", false);
+        }
+    }
+
+
+
+    public void Idle()
+    {
+        animator.speed = 0f;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMovement playerMovement;
+    [SerializeField]
+    private PlayerAnimations playerAnimations;
+
+
+    [SerializeField]
+    private MovementType movementType;
+
+
+
+    private void Start()
+    {
+        if (movementType == MovementType.Sidescroller)
+        {
+            playerMovement.IsGravityScaleActivated(true);
+        }
+        else
+        {
+            playerMovement.IsGravityScaleActivated(false);
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+
+
+        if ((x > 0.05f || x < -0.05f) || (y > 0.05f || y < -0.05f))
+        {
+            //Horizontal movement, which is common to Sidescroller and Top-Down.
+            playerAnimations.HorizontalAnimation(x);
+
+            //Determines the type of movement system
+            if (movementType == MovementType.Sidescroller)
+            {
+                playerMovement.IsGravityScaleActivated(true);
+
+                playerMovement.MovementSideScroller(x);
+            }
+            else
+            {
+                playerMovement.IsGravityScaleActivated(false);
+
+                playerAnimations.VerticalAnimation(y);
+
+                playerMovement.MovementTopDown(x, y);
+            }
+        }
+        else if ((x < 0.95f || x > -0.95f) || (y < 0.95f || y > -0.95f))
+        {
+            if (movementType == MovementType.TopDown)
+            {
+                playerMovement.Idle();
+            }
+
+            playerAnimations.Idle();
+        }
+
+
+
+        if (movementType == MovementType.Sidescroller)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerMovement.Jump();
+            }
+        }
+    }
+}
