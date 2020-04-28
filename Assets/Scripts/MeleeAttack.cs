b88916f169b913a897c60a70f82e298e3c9e6ac2
Cr8v Dev <cr8vdev@gmail.com>
2020-04-28 20:37:27 +0200

    Arena Attack System

---
+++ Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    [SerializeField]
+    private new Camera camera;
+    [SerializeField]
+    private float distance;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, camera.ScreenToWorldPoint(Input.mousePosition) - transform.position, distance);
+
+            if (ray.collider != null)
+            {
+                if (ray.collider.tag[0] == '1')
+                {
+                    ray.collider.GetComponent<Rigidbody2D>().AddForce(camera.ScreenToWorldPoint(Input.mousePosition) - transform.position, ForceMode2D.Impulse);
+                }
+            }
+        }
+    }
+}
